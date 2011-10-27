b0e23c04e9e68a27d26072742ff4f0b5ee3d3db6
Craig <kungfucraig@mgial.com>
2011-10-27 16:08:15 -0400

    Initialized

---
+++ bash/bash-ext.sh
@@ -0,0 +1,174 @@
+# This is a bash extension file that I can share with others.
+# It does not have anything in it that preclude it from being used
+# by others.
+
+# Clear the path and start over.
+export PATH=""
+
+# Set a umask.
+umask 002
+
+if [ -z $OS ]
+then
+   export OS=`/bin/uname -o`
+fi
+
+# set vi mode
+set -o vi
+
+# Setup the PATH to include a person's personal bin directory
+# and the sbin directories. It's nice to have these commands.
+export PATH=~/bin:/bin/:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin
+
+# Set the PAGER to be less
+export PAGER="less"
+
+# Change the window title of X terminals 
+case $TERM in
+	xterm*|rxvt|Eterm|eterm)
+		PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\007"'
+		;;
+	screen)
+		PROMPT_COMMAND='echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\033\\"'
+		;;
+esac
+
+
+# Setup alias for ls and deal with ls colors.
+# Only do this if the terminal is not dumb.
+if [ $TERM == "dumb" ]
+then
+   alias ls="ls -F"
+   alias ll="ls -alF"
+   alias li="ls -F"
+else
+   alias ls="ls --color=auto -F"
+   alias ll="ls --color -alF"
+   alias li="ls --color=auto -F"
+fi
+
+# Setup remove to be less destructive.
+alias rm='rm -i'
+
+# Function to find a remove files.
+# It takes one argument, then 
+function findandremove
+{
+    find . -name $1 -exec rm -f {} \;
+}
+
+# A function to find files by name
+function hunt
+{
+    find . -name $1 
+}
+
+function command-search
+{
+   oldIFS=${IFS}
+   IFS=":"
+
+   for p in ${PATH}
+   do
+      ls $p | grep $1
+   done
+
+   export IFS=${oldIFS}
+}
+
+# Set a common prompt.
+if [ -n "$PS1" ]
+then
+   # Same prompt with color or not?
+   if [ $TERM == "dumb" ]
+   then
+     export PS1="[\u@\h][\w]\n$ " 
+   else
+     export PS1="\[\033[01;32m\][\u@\h]\[\033[01;34m\][\w]\n\$ \[\033[00m\]"
+   fi
+fi
+
+# Set up environment for editing files
+#export BROWSER="/usr/bin/emacsclient --no-wait --alternate-editor=emacs"
+export EDITOR="/usr/bin/emacsclient --no-wait --alternate-editor=emacs"
+export CVSEDITOR="/usr/bin/emacsclient --alternate-editor=emacs"
+export SVN_EDITOR="/usr/bin/emacsclient --alternate-editor=emacs"
+#export EDITOR="gvim --remote"
+
+alias edit="$EDITOR"
+alias medit="managed-edit"
+
+# Some options I like for my xterms in Cygwin
+# The Windows way to do this is:
+# C:\cygwin\bin\run.exe -p /usr/X11R6/bin xterm -display 127.0.0.1:0.0 -ls +tb -j -sb -si -sk -rightbar
+alias xt="xterm -display 127.0.0.1:0.0 -ls +tb -j -sb -si -sk -rightbar &"
+
+function ccd
+{
+   x=`me -d $@`
+   if [ -n "$x" ]
+   then
+     cd $x
+   fi
+}
+
+function setdisplay
+{
+  export DISPLAY=$1:0.0
+}
+
+
+##############################################
+# GO
+#
+# Inspired by some Windows Power Shell code
+# from Peter Provost (peterprovost.org)
+#
+# Here are some examples entries:
+# work:${WORK_DIR}
+# source:${SOURCE_DIR}
+# dev:/c/dev
+# object:${USER_OBJECT_DIR}
+# debug:${USER_OBJECT_DIR}/debug
+###############################################
+export GO_FILE=~/.go_locations
+function go
+{
+   if [ -z "$GO_FILE" ]
+   then
+      echo "The variable GO_FILE is not set."
+      return
+   fi
+
+   if [ ! -e "$GO_FILE" ]
+   then
+      echo "The 'go file': '$GO_FILE' does not exist."
+      return
+   fi
+
+   dest=""
+   oldIFS=${IFS}
+   IFS=$'\n'
+   for entry in `cat ${GO_FILE}`
+   do
+      if [ "$1" = ${entry%%:*} ]
+      then
+         #echo $entry
+         dest=${entry##*:}
+         break
+      fi
+   done
+
+   if [ -n "$dest" ]
+   then
+      # Expand variables in the go file. 
+      #echo $dest
+		# make sure there are no \r characters in the .go_locations file.
+      cd `eval echo $dest`
+   else
+      echo "Invalid location, valid locations are:"
+      cat $GO_FILE
+   fi
+   export IFS=${oldIFS}
+}
+alias d='go'
---
+++ emacs/simple-dot-emacs.el
@@ -0,0 +1,269 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Options
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq inhibit-startup-message t)
+(setq tab-width 3)
+(setq make-backup-files nil) 
+(require 'font-lock)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(blink-cursor-mode 0)
+(transient-mark-mode 1)
+(tool-bar-mode 0)
+(setq-default indent-tabs-mode nil)
+(setq next-line-add-newlines nil)
+(server-start)
+(put 'set-goal-column 'disabled nil)
+(setq visible-bell t)
+
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Viper Mode Stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq viper-mode t)
+(setq viper-ex-style-editing nil) ; can backspace past start of insert/line
+(setq viper-inhibit-startup-message 't)
+(setq viper-expert-level '5)
+(require 'viper)
+;;(setq vimpulse-experimental nil) ; turn off bleeding edge features
+(require 'vimpulse)
+(require 'redo)
+
+(setq-default viper-auto-indent t)
+(setq-default tab-width 3)
+(setq-default viper-shift-width 3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Map some keys
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key [f6] 'copy-to-register)
+(global-set-key [f7] 'insert-register)
+(global-set-key [?\C-x?\h] 'eshell)
+(global-set-key [?\C-x?\C-=] 'revert-buffer)
+(global-set-key "\C-x\-" 'bury-buffer)
+(global-set-key "\C-x\g" 'goto-line)
+(global-set-key [?\M-n] 'scroll-up)
+(global-set-key [?\M-p] 'scroll-down)
+(global-set-key [C-kp-home] 'beginning-of-buffer)
+(global-set-key [C-home] 'beginning-of-buffer)
+(global-set-key [C-kp-end] 'end-of-buffer)
+(global-set-key [C-end] 'end-of-buffer)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++ Stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun cpp-source-file-header (fileName)
+  (interactive "bBuffer Name: ")
+  (shell-command 
+   (concat "cg cs -m classname="
+           (if (or (string= (substring fileName -2) "qh")
+                   (string= (substring fileName -2) "cc"))
+               (substring fileName 0 -3)
+               (substring fileName 0 -2))
+           )
+   1
+   )
+  )
+
+(defun cpp-include-file-header (fileName) 
+  (interactive "bBuffer Name: ")
+  (shell-command (concat "cg ch -m classname="
+                         (if (string= (substring fileName -2) "qh")
+                             (substring fileName 0 -3)
+                           (substring fileName 0 -2))
+                         )
+                 1
+                 )
+  (beginning-of-buffer))
+
+(defun cpp-function-header ()
+  (interactive)
+  (shell-command (concat "cg cf") 1))
+
+(defun xml-file-header (fileName) 
+  (interactive "bBuffer Name: ")
+  (shell-command (concat "cg xml -m filename=" fileName)
+                 1 )
+  (end-of-buffer))
+
+
+;; Limitation here is that this only works with
+;; files that have a two character extension.
+(defun insert-classname ()
+  (interactive)
+  (let*
+      ((path
+    (split-string (buffer-file-name (current-buffer)) "/"))
+       (filename (nth (- (length path) 1) path))
+       (fileNoExt (split-string filename "\\."))
+       )
+    (insert (car fileNoExt))))
+
+
+;; You should have the stl container
+;; declaration string in register
+;; 1
+(defun stl-for-loop ()
+  (interactive)
+  (indent-according-to-mode)
+  (let* (
+         ;;(r1 (get-register 49))
+         (r1 (car kill-ring))
+         (sp (split-string r1))
+    (type (car sp))
+    (var (substring (cadr sp) 0 (- (length (cadr sp)) 1))))
+    
+    (insert (concat "const " type "::iterator end = " var ".end();\n"))
+    (indent-for-tab-command)
+    (insert (concat "for ( " type "::iterator iter = " var ".begin();\n"))
+    (indent-for-tab-command)
+    (insert "iter != end;\n")
+    (indent-for-tab-command)
+    (insert "++iter )\n")
+    (insert "   {\n\n   }\n")
+    (previous-line 2)
+    (indent-for-tab-command)
+    ))
+
+
+(defun add-data-class-function ()
+  (interactive)
+  (let* (
+         ;;(r1 (get-register 49))
+         (r1 (car kill-ring)))
+    (search-backward ";")
+    (search-backward ";")
+    (search-backward "(")
+    (backward-word 1)
+    (let* ((startPoint (point)))
+      (forward-word 1)
+      (let ((endPoint (point)))
+        (copy-to-register 9 startPoint endPoint)
+        ))
+    (let ((r9 (get-register 9)))
+      (search-forward (concat "::" r9)))
+
+    (search-forward "{")
+    (end-of-defun)
+    (insert "\n")
+    (cpp-function-header)
+    (insert r1)
+    (beginning-of-line)
+    (indent-for-tab-command)    
+    (forward-word 1)
+    (forward-char 1)
+    (insert-classname)
+    (insert "::Data::")
+    (end-of-line)
+    (backward-delete-char-untabify 1)
+    (insert "\n{\n\n}\n")
+    (previous-line 2)
+    (indent-for-tab-command)
+    (inset "Result r;\n")
+    (indent-for-tab-command)
+    (insert (concat "const char* fname =\"" "\";\n"))
+    (indent-for-tab-command)
+    ))
+
+
+(defun insert-success ()
+  (interactive)
+  (indent-for-tab-command)
+  (insert "return Result(CMT_SUCCESS);"))
+
+;; Don't use tabs, use spaces.
+(setq indent-tabs-mode nil)
+
+(setq prefixes '("C_" "W_" "KRN_"
+                        "EVT_" "J_" "UTL_"
+                        "Q" "ABS_" "CAD_" "EVT_"
+                        "FLD_" "INR_" "SMT_" "PRO_" "SYC_"))
+
+(setq my-c-style
+      `(
+        (c-cleanup-list . (scope-operator
+                  empty-defun-braces
+                  defun-close-semi))
+        (c-offsets-alist . (
+                            (arglist-close . c-lineup-arglist)
+                            (arglist-cont-nonempty . c-lineup-arglist)
+                            (inclass . ++)
+                            (inline-open . 0)
+                            (access-label . -)
+                            (substatement-open . 0)
+                            ))
+        
+        (c-access-key . "\\<\\(signals\\|\\(public\\|protected\\|private\\)\\([     ]+slots\\)?\\)\\>:")
+        
+        (c-protection-key . "\\<\\(public\\|public slot\\|protected\\|protected slot\\|private\\|private slot\\)\\>")
+        (c-C++-access-key . "\\<\\(signals\\|public\\|protected\\|private\\|public slots\\|protected slots\\|private slots\\)\\>[ \t]*:")
+        
+        ;;,(append (mapcar (lambda (x) (concat x "_[^ :<(]*")) prefixes) 
+        ;; This is some CoMeT specific stuff.
+        (c++-font-lock-extra-types . ,(append (mapcar (lambda (x) (concat x "[^ :<(]*")) prefixes) '("Data" "std::list" "std::set" "set::map" "std::multimap" "std::vector" "std::stack" "iterator" "const_iterator" "size_t")))
+        
+        (c-echo-syntactic-information-p . t )  ;; what's this do?
+        ))
+
+(add-hook 
+ 'c-mode-common-hook
+ (lambda ()
+   (c-add-style "my-c-style" my-c-style t)
+   
+   ;; Set up some keys.
+   (local-set-key "\C-c\C-f" 'cpp-function-header)
+   (local-set-key "\C-c\C-j" 'insert-classname)
+   (local-set-key "\C-c\C-l" 'stl-for-loop)
+   (local-set-key "\C-cl" 'add-data-class-function)
+   (local-set-key "\C-cs" 'insert-success)
+   
+   ;; These functions must be after the call to c-add-style.
+   (setq c-basic-offset 3)  
+   ;; this seems to be key for "for loop" indenting
+   (setq indent-tabs-mode nil)
+   (modify-syntax-entry ?_ "w")))
+
+
+(setq auto-mode-alist 
+      (append '(("\\.C\\'" . c++-mode)  
+                ("\\.cxx\\'" . c++-mode)
+                ("\\.h\\'" . c++-mode)  
+                ("\\.hxx\\'" . c++-mode)
+                ("\\.qh\\'" . c++-mode)
+                ("\\.hpp\\'" . c++-mode)
+                ("\\.cc\\'" . c++-mode)
+                ("\\.c\\'" . c++-mode)
+                ("\\.template\\'" . c++-mode))
+              auto-mode-alist))
+
+
+(defun insert-check-result ()
+  (interactive)
+  (insert "CHECK_RESULT_RETURN_ON_FAILURE(r);"))
+
+(add-hook 
+ 'c-mode-common-hook
+ (lambda ()
+   (local-set-key [f9] 'code-compile-buffer)
+   (local-set-key "\C-c\C-r" 'insert-check-result)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MATLAB Stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
+ (add-to-list
+  'auto-mode-alist
+  '("\\.m$" . octave-mode))
+ (setq octave-indent-function t)
